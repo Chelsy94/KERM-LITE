177f58a74269b7ff426ebf9d05513af6736aef27
Chelsy94 <amadouchelsy@gmail.com>
2024-12-02 15:13:50 +0100

    Update config.js

--- config.js
+++ config.js
@@ -16,7 +16,7 @@ module.exports = {
    AUTO_REACT: toBool(process.env.AUTO_REACT) || true,
 
    // String Values
-   SESSION_ID: process.env.SESSION_ID || "KermLite~UCFnYu22", // Put your session
+   SESSION_ID: process.env.SESSION_ID || "KermLite~Y9aqjPSF", // Put your session
    HANDLERS: process.env.HANDLER === "false" || process.env.HANDLER === "null" ? "^" : "[!]",
    BOT_INFO: process.env.BOT_INFO || "CONSTIPATION⚡️", // YourName;BotName;Image/VideoLink
    SUDO: process.env.SUDO || "237653065230",
